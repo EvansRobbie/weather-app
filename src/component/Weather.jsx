1e2bb82689b3c3d8e3df9efb0e1604d057e8dc33
EvansRobbie <evansitoden94@gmail.com>
2023-02-14 18:33:55 +0300

    final commit

--- src/component/Weather.jsx
+++ src/component/Weather.jsx
@@ -7,28 +7,38 @@ import WeatherItem from './WeatherItem'
 
 const Weather = () => {
     const [weather, setWeather] = useState([])
+    const [cityImage, setCityImage] = useState([])
     const [city, setCity] = useState('')
     const [loading, setLoading] = useState(false)
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_PUBLIC_API_KEY}`
-    console.log(process.env.REACT_APP_PUBLIC_API_KEY)
+    const urlCities =`https://api.teleport.org/api/urban_areas/slug:${city.toLowerCase()}/images/`
+    // console.log(process.env.REACT_APP_PUBLIC_API_KEY)
     const handleSubmit =(e) =>{
         e.preventDefault()
         setLoading(true)
-        try{
-            axios.get(url)
+       
+         axios.get(url)
         .then(res =>{setWeather(res.data)})
-        
-        }
-        catch(e){
-            console.log(e.message)
-        }
+        axios.get(urlCities)
+        .then(res =>{
+             setCityImage(res.data.photos[0].image.mobile)
+            
+        })
+
+        .catch(AxiosError => {
+            if (AxiosError.response && AxiosError.response.status === 404) {
+              console.log("City Image not found. Please check your city spelling and try again. NB: The City image might me missing from the API");
+            } else {
+              console.AxiosError(AxiosError);
+            }
+          });
         setCity('')
         setLoading(false)
     }
     // useEffect (() =>{
     //     handleSubmit()
     // }, [country])
-    // console.log(weather)
+    console.log(cityImage)
     if (loading){
         return <Spinner/>
     } 
@@ -37,9 +47,9 @@ const Weather = () => {
         return (
           <div className='relative '>
               
-              <img className='w-full h-screen object-cover ' src="https://cdn.pixabay.com/photo/2018/03/01/16/38/nature-3191091__340.jpg" alt="" />
+              <img className='w-full h-screen object-cover ' src= {cityImage.length <= 0 ? 'https://cdn.pixabay.com/photo/2018/03/01/16/38/nature-3191091__340.jpg' : cityImage} alt="/" />
              
-                 
+             
                   <div className='absolute top-0 left-0 bg-black/30 h-screen w-full z-10 opacity-100' />
                   <div className='absolute top-0 left-0 w-full h-screen my-8 opacity-100 z-20'>
                   {/* <div className='flex items-center justify-center py-16 '> */}
--- src/component/WeatherItem.jsx
+++ src/component/WeatherItem.jsx
@@ -21,13 +21,14 @@ const WeatherItem = ({weather}) => {
         <div className='relative max-w-[500px] mx-auto w-full h-[85vh] flex flex-col justify-between'>
             <div className='flex h-full w-full justify-between py-4'>
                 {weatherElement}
-                <p className='text-slate-200 text-6xl my-7 mx-2 '>{weather.main.temp.toFixed(0)}&#176;</p>
+                {/* convert kelvin to degree celcius -273.15 */}
+                <p className='text-slate-200 text-6xl my-7 mx-2 '>{(weather.main.temp-273.25).toFixed(0)}&#176;</p>
             </div>
             <div className='bg-slate-900/70 rounded-sm flex flex-col h-auto w-full justify-between '>
                 <p className='text-center text-slate-200 py-4 text-lg font-medium'>Weather in {weather.name} </p>
                 <div className='flex items-center justify-between w-full px-4 py-8'>
                     <div className='text-slate-200'>
-                    <p className='font-bold'>{weather.main.feels_like.toFixed(0)}&#176;</p>
+                    <p className='font-bold'>{weather.main.feels_like.toFixed(0)-273.25}&#176;</p>
                     <h1 className='font-medium'>Feels like</h1>
                     </div>
                     <div className='text-slate-200'>
